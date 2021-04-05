de6343d4bf1c365bdcd2a08c78a2c1221c5be246
a.molnar <a.molnar@student.tue.nl>
2021-04-06 00:38:57 +0200

    Implemented bluetooth connection

--- rc_software.py
+++ rc_software.py
@@ -1,9 +1,11 @@
 from json import dumps, loads
 from threading import Thread
 from socket import socket, AF_INET, SOCK_STREAM, SOCK_DGRAM, error as socket_error
+from bluetooth import BluetoothSocket, RFCOMM, PORT_ANY, SERIAL_PORT_CLASS, SERIAL_PORT_PROFILE, advertise_service, BluetoothError
 from requests import get, post
 from random import randint
 from subprocess import run, check_output
+from sys import maxsize
 
 from controller import Controller
 
@@ -19,7 +21,7 @@ class RcCar:
         self.db_id = 1  # TODO: from file (file created during init)
 
         self.is_lan_active = self.__setup_lan_connection()
-        self.is_bt_active = False  # bluetooth setup here
+        self.is_bt_active = self.__setup_bt_connection()
 
     @staticmethod
     def __get_ip() -> str:
@@ -70,26 +72,46 @@ class RcCar:
                 "available": 1
             }
         )  # TODO: set timeout like 1 minute to invalidate port
-        print(f"port: {local_port}, ssid: {RcCar.__get_ssid()}")
+        print(f"LAN port: {local_port}, ssid: {RcCar.__get_ssid()}")
 
         self.lan_socket.listen()
-        self.lan_socket.settimeout(3)
+        self.lan_socket.settimeout(1)
         return True
 
-    def __receive_lan_connection(self) -> None:
-        lan_connection_made = False
-        print("Accepting LAN Connection...")
+    def __setup_bt_connection(self) -> bool:
+        run('sh/bt_disc_on', shell=True)
+        self.bt_socket = BluetoothSocket(RFCOMM)
+        self.bt_socket.bind(("", PORT_ANY))
+        self.bt_socket.settimeout(1)
+        self.bt_socket.listen(maxsize)
+
+        port = self.bt_socket.getsockname()[1]
+
+        uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+
+        advertise_service(
+            self.bt_socket,
+            "RC_car_raspberrypi",
+            service_id=uuid,
+            service_classes=[uuid, SERIAL_PORT_CLASS],
+            profiles=[SERIAL_PORT_PROFILE]
+        )
+        print(f"BT port: {port}, uuid: {uuid}")
+        return True
+
+    def __receive_connection(self, server_socket) -> None:
+        connection_made = False
+        print(f"Accepting {'LAN' if server_socket.getsockname()[0] == '0.0.0.0' else 'BT'} Connection...")
         while not self.is_connection_alive:
             try:
-                self.receiving_socket, _ = self.lan_socket.accept()
-                self.sending_socket, _ = self.lan_socket.accept()
-                lan_connection_made = True
+                self.message_socket, _ = server_socket.accept()
+                connection_made = True
                 self.is_connection_alive = True
             except socket_error:
                 pass
 
-        if not lan_connection_made:
-            print("returning, since bluetooth connected..")
+        if not connection_made:
+            print(f"returning, since {'LAN' if server_socket.getsockname()[0] != '0.0.0.0' else 'BT'} connected..")
             return
 
         # TODO: better hashing algorithm
@@ -97,26 +119,19 @@ class RcCar:
         with open('passwd', 'rb') as f:
             self.password = f.readline()
 
-        received_password = self.receiving_socket.recv(1024)
+        received_password = self.message_socket.recv(1024)
         print(f'pwd: {self.password}\nrec: {received_password}')
         if self.password == received_password:
             print('...GRANTED')
-            self.sending_socket.sendall('GRANTED\n'.encode())
+            self.message_socket.sendall('GRANTED\n'.encode())
             self.is_connection_alive = True
         else:
             print('...REJECTED')  # TODO: error handling
 
-    def __receive_bt_connection(self) -> None:
-        raise NotImplementedError
-
     def __close_sockets(self):
         try:
             print("closing connections")
-            self.sending_socket.close()
-        except Exception:
-            pass
-        try:
-            self.receiving_socket.close()
+            self.message_socket.close()
         except Exception:
             pass
 
@@ -129,11 +144,11 @@ class RcCar:
                 bt_thread = None
 
                 if self.is_lan_active:
-                    lan_thread = Thread(target=self.__receive_lan_connection)
+                    lan_thread = Thread(target=self.__receive_connection, args=(self.lan_socket,))
                     lan_thread.start()
 
                 if self.is_bt_active:
-                    bt_thread = Thread(target=self.__receive_bt_connection)
+                    bt_thread = Thread(target=self.__receive_connection, args=(self.bt_socket,))
                     bt_thread.start()
 
                 if lan_thread:
@@ -148,6 +163,7 @@ class RcCar:
                 update_thread.join()
             except Exception as e:
                 print(e)
+                self.is_connection_alive = False
             finally:
                 self.__close_sockets()
 
@@ -163,7 +179,7 @@ class RcCar:
     def receive_commands(self) -> None:
 
         while True:
-            data = self.receiving_socket.recv(1024).decode()
+            data = self.message_socket.recv(1024).decode()
             if not data:
                 self.is_connection_alive = False
                 break
@@ -177,11 +193,7 @@ class RcCar:
 
         while self.is_connection_alive:
             message = dumps(self.controller.get_values()) + '\n'
-            self.sending_socket.sendall(message.encode())
-            try:
-                _ = self.sending_socket.recv(1024)
-            except:
-                pass  # There is a chance the socket closes on the other side before this call gets made
+            self.message_socket.sendall(message.encode())
             sleep(0.05)  # distance sensor
 
 
