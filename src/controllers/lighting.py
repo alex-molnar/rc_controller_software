3422c6f3314202c3dd98b95d97832b3ccff26884
a.molnar <a.molnar@student.tue.nl>
2021-04-27 17:05:00 +0200

    Refactored lights, so now you can index with the lights off as well

--- src/controllers/lighting.py
+++ src/controllers/lighting.py
@@ -10,11 +10,10 @@ HAZARD_WARNING = 'hazard_warning'
 LIGHTS = 'lights'
 BACKWARD = 'backward'
 
-LIGHTS_OFF = 0
-LIGHTS_ON = 1
-HAZARD_ON = 2
-RIGHT_ON = 3
-LEFT_ON = 4
+INDICATORS_OFF = 0
+HAZARD_ON = 1
+RIGHT_ON = 2
+LEFT_ON = 3
 
 
 class Lights:
@@ -31,60 +30,34 @@ class Lights:
         self.right_indicator = LED(right_indicator_pin)
         self.left_indicator = LED(left_indicator_pin)
 
+        for light in [*self.front_lights, *self.back_lights, self.left_indicator, self.right_indicator]:
+            light.off()
+
         self.private_states = defaultdict(bool)
         self.public_states = defaultdict(bool)
 
-        self.state = LIGHTS_OFF
+        self.light_state = False
+        self.indicator_state = INDICATORS_OFF
 
         self.breaking = False
         self.logger = getLogger('rc_controller')
 
     def handle_lights(self, data: Dict[str, bool]) -> None:
-        if self.state == LIGHTS_OFF:
-            if data[LIGHTS]:
-                self.__to_li_on()
-                self.__turn_lights_on()
-        elif not data[LIGHTS]:
-            self.__to_li_off()
-            self.__turn_lights_off()
-        elif self.state == LIGHTS_ON:
-            if data[HAZARD_WARNING]:
-                self.__to_ha_on()
-            elif data[R_INDICATOR]:
-                self.__to_ri_on()
-            elif data[L_INDICATOR]:
-                self.__to_le_on()
-        elif self.state == HAZARD_ON:
-            if not data[HAZARD_WARNING]:
-                self.__to_li_on()
-        elif self.state == RIGHT_ON:
-            if data[HAZARD_WARNING]:
-                self.__to_ha_on()
-            elif data[L_INDICATOR]:
-                self.__to_le_on()
-            elif not data[R_INDICATOR]:
-                self.__to_li_on()
-        elif self.state == LEFT_ON:
-            if data[HAZARD_WARNING]:
-                self.__to_ha_on()
-            elif data[R_INDICATOR]:
-                self.__to_ri_on()
-            elif not data[L_INDICATOR]:
-                self.__to_li_on()
+        if self.light_state != data[LIGHTS]:
+            self.toggle_lights()
+            self.light_state = data[LIGHTS]
+            self.public_states[LIGHTS] = data[LIGHTS]
 
-        if self.private_states[L_INDICATOR] != self.public_states[L_INDICATOR]:
-            self.__toggle_left_indicator()
-            self.private_states[L_INDICATOR] = self.public_states[L_INDICATOR]
+        if self.indicator_state != HAZARD_ON and data[HAZARD_WARNING]:
+            self.turn_hazard_warning_on()
+        elif self.indicator_state not in [HAZARD_ON, RIGHT_ON] and data[R_INDICATOR]:
+            self.turn_right_indicator_on()
+        elif self.indicator_state not in [HAZARD_ON, LEFT_ON] and data[L_INDICATOR]:
+            self.turn_left_indicator_on()
+        elif not any([data[index] for index in [HAZARD_WARNING, R_INDICATOR, L_INDICATOR]]):
+            self.turn_indicators_off()
 
-        if self.private_states[R_INDICATOR] != self.public_states[R_INDICATOR]:
-            self.__toggle_right_indicator()
-            self.private_states[R_INDICATOR] = self.public_states[R_INDICATOR]
-
-        if self.private_states[HAZARD_WARNING] != self.public_states[HAZARD_WARNING]:
-            self.__syncronize_indicators()
-            self.private_states[HAZARD_WARNING] = self.public_states[HAZARD_WARNING]
-
-        if self.state != LIGHTS_OFF and self.breaking != data[BACKWARD]:
+        if self.light_state and self.breaking != data[BACKWARD]:
             for light in self.back_lights:
                 light.value = 1 if data[BACKWARD] else 0.1
             self.breaking = data[BACKWARD]
@@ -92,73 +65,42 @@ class Lights:
     def get_data(self) -> ItemsView[Any, bool]:
         return self.public_states.items()
 
-    def __to_li_off(self) -> None:
-        self.logger.debug('Lights turned off')
-        self.state = LIGHTS_OFF
-        self.public_states[LIGHTS] = False
-        self.public_states[HAZARD_WARNING] = False
-        self.public_states[R_INDICATOR] = False
-        self.public_states[L_INDICATOR] = False
-
-    def __to_li_on(self) -> None:
-        self.logger.debug('Lights turned on')
-        self.state = LIGHTS_ON
-        self.public_states[LIGHTS] = True
-        self.public_states[HAZARD_WARNING] = False
-        self.public_states[R_INDICATOR] = False
-        self.public_states[L_INDICATOR] = False
-
-    def __to_ri_on(self) -> None:
+    def turn_right_indicator_on(self) -> None:
         self.logger.debug('Right indicator turned on')
-        self.state = RIGHT_ON
-        self.public_states[LIGHTS] = True
+        self.indicator_state = RIGHT_ON
         self.public_states[HAZARD_WARNING] = False
         self.public_states[R_INDICATOR] = True
         self.public_states[L_INDICATOR] = False
+        self.left_indicator.off()
+        self.right_indicator.blink(non_blocking=True)
 
-    def __to_le_on(self) -> None:
+    def turn_left_indicator_on(self) -> None:
         self.logger.debug('Left indicator turned on')
-        self.state = LEFT_ON
-        self.public_states[LIGHTS] = True
+        self.indicator_state = LEFT_ON
         self.public_states[HAZARD_WARNING] = False
         self.public_states[R_INDICATOR] = False
         self.public_states[L_INDICATOR] = True
+        self.right_indicator.off()
+        self.left_indicator.blink(non_blocking=True)
 
-    def __to_ha_on(self) -> None:
+    def turn_hazard_warning_on(self) -> None:
         self.logger.debug('Hazard warning turned on')
-        self.state = HAZARD_ON
-        self.public_states[LIGHTS] = True
+        self.indicator_state = HAZARD_ON
         self.public_states[HAZARD_WARNING] = True
-        self.public_states[R_INDICATOR] = True
-        self.public_states[L_INDICATOR] = True
+        self.right_indicator.off()
+        self.left_indicator.off()
+        self.right_indicator.blink(non_blocking=True)
+        self.left_indicator.blink(non_blocking=True)
 
-    def __turn_lights_on(self) -> None:
-        for light in self.front_lights:
-            light.on()
+    def turn_indicators_off(self) -> None:
+        self.logger.debug('Hazard warning turned on')
+        self.indicator_state = INDICATORS_OFF
+        self.public_states[HAZARD_WARNING] = False
+        self.public_states[R_INDICATOR] = False
+        self.public_states[L_INDICATOR] = False
+        self.right_indicator.off()
+        self.left_indicator.off()
 
-        for light in self.back_lights:
-            light.on()
-            light.value = 0.1
-
-    def __turn_lights_off(self) -> None:
+    def toggle_lights(self) -> None:
         for light in [*self.front_lights, *self.back_lights]:
-            light.off()
-
-    def __toggle_left_indicator(self) -> None:
-        if self.private_states[L_INDICATOR]:
-            self.left_indicator.off()
-        else:
-            self.left_indicator.blink(times=LED.INF, on_time=0.3, off_time=0, fade_in_time=0.5, fade_out_time=0.5, non_blocking=True)
-
-    def __toggle_right_indicator(self) -> None:
-        if self.private_states[R_INDICATOR]:
-            self.right_indicator.off()
-        else:
-            self.right_indicator.blink(times=LED.INF, on_time=0.3, off_time=0, fade_in_time=0.5, fade_out_time=0.5, non_blocking=True)
-
-    def __syncronize_indicators(self) -> None:
-        if not self.private_states[HAZARD_WARNING]:
-            self.right_indicator.off()
-            self.left_indicator.off()
-            self.right_indicator.blink(times=LED.INF, on_time=0.3, off_time=0, fade_in_time=0.5, fade_out_time=0.5, non_blocking=True)
-            self.left_indicator.blink(times=LED.INF, on_time=0.3, off_time=0, fade_in_time=0.5, fade_out_time=0.5, non_blocking=True)
+            light.toggle(1 if light in self.front_lights else 0.1)
