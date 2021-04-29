cee038c3bc47297f48992a4418a72d2aa0e9c0c0
a.molnar <a.molnar@student.tue.nl>
2021-04-29 10:38:19 +0200

    deployment script now automatically changes windows line endings

--- sh/deployment.sh
+++ sh/deployment.sh
@@ -45,6 +45,7 @@ old_path=$( (echo "$PATH_TO_ROOT_DIR/src"|sed -r 's/([\$\.\*\/\[\\^])/\\\1/g'|se
 new_path=$( (echo '/opt/raspberrypi_rc_car'|sed -r 's/([\$\.\*\/\[\\^])/\\\1/g'|sed 's/[]]/\[]]/g')>&1)
 sed -i -e "s/$old_path/$new_path/g" "$PATH_TO_ROOT_DIR/raspberrypi_rc_car/rc_software.py" || exit_notify "Editing path to config file failed."
 sed -i -e "s/$old_path/$new_path/g" "$PATH_TO_ROOT_DIR/raspberrypi_rc_car/sockets/bt_socket.py" || exit_notify "Editing path to config file failed."
+sed -i 's/\r$//' sh/installScript.sh
 
 tar -czvf raspberrypi_rc_car.tar.gz raspberrypi_rc_car >/dev/null 2>&1 || exit_notify "Compressing directory failed"
 password=$(cat < passwd)
